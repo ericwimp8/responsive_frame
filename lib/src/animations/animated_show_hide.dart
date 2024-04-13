adfc600467c2722cf15fbe80132d260602750047
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-13 18:03:46 +0930

    remove unneeded arguments

--- lib/src/animations/animated_show_hide.dart
+++ lib/src/animations/animated_show_hide.dart
@@ -4,7 +4,6 @@ import 'package:responsive_frame/responsive_frame.dart';
 class AnimatedShowHide extends StatelessWidget {
   const AnimatedShowHide({
     required this.child,
-    required this.show,
     this.duration = const Duration(milliseconds: 180),
     this.switchInCurve = Curves.linear,
     this.switchOutCurve = Curves.linear,
@@ -14,12 +13,12 @@ class AnimatedShowHide extends StatelessWidget {
   });
 
   final Duration duration;
-  final Widget child;
+  final Widget? child;
   final Curve switchInCurve;
   final Curve switchOutCurve;
   final Axis axis;
   final double axisAlignment;
-  final bool show;
+
   @override
   Widget build(BuildContext context) {
     return FrameAnimatedSwitcher(
@@ -28,7 +27,7 @@ class AnimatedShowHide extends StatelessWidget {
       switchOutCurve: switchOutCurve,
       axis: axis,
       axisAlignment: axisAlignment,
-      child: show ? child : const SizedBox(),
+      child: child ?? const SizedBox(),
     );
   }
 }
