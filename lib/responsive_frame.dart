ca4d23e616ef65c94564746e5319e455f935cd45
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-13 18:02:28 +0930

    update exports

--- lib/responsive_frame.dart
+++ lib/responsive_frame.dart
@@ -1,6 +1,8 @@
-library responsive_frame;
-
 export 'responsive_frame.dart';
+export 'src/animated_resposive_frame.dart';
+export 'src/animations/animated_show_hide.dart';
+export 'src/animations/frame_animated_switcher.dart';
+export 'src/animations/size_fade_transition.dart';
 export 'src/break_points.dart';
 export 'src/break_points_granular.dart';
 export 'src/frame.dart';
