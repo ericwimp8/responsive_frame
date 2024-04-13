a117b2c79455b6d7f57779ef9e11ee91b1892fbd
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-13 16:54:49 +0930

    add arguments

--- lib/src/frame_animated_switcher.dart
+++ lib/src/frame_animated_switcher.dart
@@ -5,20 +5,30 @@ class FrameAnimatedSwitcher extends StatelessWidget {
   const FrameAnimatedSwitcher({
     required this.child,
     super.key,
+    this.duration = const Duration(milliseconds: 180),
+    this.switchInCurve = Curves.linear,
+    this.switchOutCurve = Curves.linear,
+    this.axis = Axis.vertical,
   });
-
+  final Duration duration;
   final Widget child;
+  final Curve switchInCurve;
+  final Curve switchOutCurve;
+  final Axis axis;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedSwitcher(
+      duration: duration,
+      switchInCurve: switchInCurve,
+      switchOutCurve: switchOutCurve,
       transitionBuilder: (child, animation) {
         return SizeFadeTransition(
+          axis: axis,
           animation: animation,
           child: child,
         );
       },
-      duration: const Duration(milliseconds: 300),
       child: child,
     );
   }
