f2505ebf975cf7546a7963268582099f43213244
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-14 08:57:01 +0930

    fix: Widget now only animates when being removed from tree, not when wiget changes to another widget.

--- lib/src/animations/animated_show_hide.dart
+++ lib/src/animations/animated_show_hide.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_frame/responsive_frame.dart';
 
-class AnimatedShowHide extends StatelessWidget {
+class AnimatedShowHide extends StatefulWidget {
   const AnimatedShowHide({
     required this.child,
+    this.show = true,
     this.duration = const Duration(milliseconds: 180),
     this.switchInCurve = Curves.linear,
     this.switchOutCurve = Curves.linear,
@@ -11,7 +11,7 @@ class AnimatedShowHide extends StatelessWidget {
     this.axisAlignment = -1,
     super.key,
   });
-
+  final bool show;
   final Duration duration;
   final Widget? child;
   final Curve switchInCurve;
@@ -20,14 +20,48 @@ class AnimatedShowHide extends StatelessWidget {
   final double axisAlignment;
 
   @override
+  State<AnimatedShowHide> createState() => _AnimatedShowHideState();
+}
+
+class _AnimatedShowHideState extends State<AnimatedShowHide>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _animation = _controller!.drive(Tween<double>(begin: 0, end: 1));
+    _controller!.forward();
+    super.initState();
+  }
+
+  Widget? _outGoingChild;
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant AnimatedShowHide oldWidget) {
+    _outGoingChild = oldWidget.child;
+    if (widget.child == null) {
+      _controller?.reverse();
+    } else {
+      _controller?.forward();
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FrameAnimatedSwitcher(
-      duration: duration,
-      switchInCurve: switchInCurve,
-      switchOutCurve: switchOutCurve,
-      axis: axis,
-      axisAlignment: axisAlignment,
-      child: child ?? const SizedBox(),
+    return SizeTransition(
+      axisAlignment: widget.axisAlignment,
+      axis: widget.axis,
+      sizeFactor: _animation,
+      child: widget.child ?? _outGoingChild,
     );
   }
 }
