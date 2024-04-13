1c8842fff2768e2105e31c1e6c9228c3aa8dbc3e
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-13 17:13:29 +0930

    refactor into animations folder

---
+++ lib/src/animations/frame_animated_switcher.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_frame/responsive_frame.dart';
+
+class FrameAnimatedSwitcher extends StatelessWidget {
+  const FrameAnimatedSwitcher({
+    required this.child,
+    super.key,
+    this.duration = const Duration(milliseconds: 180),
+    this.switchInCurve = Curves.linear,
+    this.switchOutCurve = Curves.linear,
+    this.axis = Axis.vertical,
+    this.axisAlignment = -1,
+  });
+  final Duration duration;
+  final Widget child;
+  final Curve switchInCurve;
+  final Curve switchOutCurve;
+  final Axis axis;
+  final double axisAlignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: duration,
+      switchInCurve: switchInCurve,
+      switchOutCurve: switchOutCurve,
+      transitionBuilder: (child, animation) {
+        return SizeFadeTransition(
+          axisAlignment: axisAlignment,
+          axis: axis,
+          animation: animation,
+          child: child,
+        );
+      },
+      child: child,
+    );
+  }
+}
