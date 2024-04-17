9f7505ccc5215f4980416fb5c780fb5cf683771f
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-17 11:09:25 +0930

    fix remove redundant code

--- lib/src/responsive_frame_layout.dart
+++ lib/src/responsive_frame_layout.dart
@@ -85,61 +85,6 @@ class _ResponsiveFrameLayoutState extends State<ResponsiveFrameLayout> {
   }
 }
 
-class FrameConfig {
-  const FrameConfig({
-    required this.body,
-    this.animations = true,
-    this.top,
-    this.topHeight = kDefaultVerticalEndHeight,
-    this.bodyTop,
-    this.bodyTopHeight = kDefaultVerticalEndHeight,
-    this.bodyAlignment = Alignment.topCenter,
-    this.bodyMaxWidth = 950,
-    this.bodyMinWidth = 0,
-    this.leftEnd,
-    this.leftEndTop,
-    this.leftEndMaxWidth = kDefaultHorizontalEndWidth,
-    this.leftEndMinWidth = kDefaultHorizontalEndWidth,
-    this.leftEndTopHeight = kDefaultVerticalEndHeight,
-    this.leftEndFillVertical = true,
-    this.rightEnd,
-    this.rightEndTop,
-    this.rightEndMaxWidth = kDefaultHorizontalEndWidth,
-    this.rightEndMinWidth = kDefaultHorizontalEndWidth,
-    this.rightEndTopHeight = kDefaultVerticalEndHeight,
-    this.rightEndFillVertical = true,
-    this.bodyBottom,
-    this.bodyBottomHeight = kDefaultVerticalEndHeight,
-    this.bottom,
-    this.bottomHeight = kDefaultVerticalEndHeight,
-  });
-  final bool animations;
-  final List<FrameBodyListChild> body;
-  final Widget? top;
-  final double topHeight;
-  final Widget? bodyTop;
-  final double bodyTopHeight;
-  final Alignment bodyAlignment;
-  final double bodyMaxWidth;
-  final double bodyMinWidth;
-  final Widget? leftEnd;
-  final Widget? leftEndTop;
-  final double leftEndMaxWidth;
-  final double leftEndMinWidth;
-  final bool leftEndFillVertical;
-  final double leftEndTopHeight;
-  final Widget? rightEnd;
-  final Widget? rightEndTop;
-  final double rightEndMaxWidth;
-  final double rightEndMinWidth;
-  final bool rightEndFillVertical;
-  final double rightEndTopHeight;
-  final Widget? bodyBottom;
-  final double bodyBottomHeight;
-  final Widget? bottom;
-  final double bottomHeight;
-}
-
 class Textbox extends StatefulWidget {
   const Textbox({
     super.key,
