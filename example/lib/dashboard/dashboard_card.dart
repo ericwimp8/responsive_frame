7ccd07d11fb8dd770300498a41bfcea7d8fd7c67
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-04-23 05:12:37 +0930

    Fix: Remove unused import

--- example/lib/dashboard/dashboard_card.dart
+++ example/lib/dashboard/dashboard_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:responsive_frame/responsive_frame.dart';
 
 class DashboardCard extends StatelessWidget {
