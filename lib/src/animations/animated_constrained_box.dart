4cfc6638ebf0c75244c6f32c108b52cec142bf19
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-05-08 14:53:22 +0930

    Added documentation.

--- lib/src/animations/animated_constrained_box.dart
+++ lib/src/animations/animated_constrained_box.dart
@@ -3,8 +3,58 @@ import 'package:flutter/material.dart';
 import 'package:responsive_frame/responsive_frame.dart';
 import 'package:responsive_frame/src/animations/animation_constants.dart';
 
+/// A widget that animates its constraints implicitly.
+///
+/// The `AnimatedConstrainedBox` widget allows you to animate the constraints
+/// of its child widget over a given duration. It takes a [BoxConstraints] object
+/// as its constraints parameter, which defines the constraints to be applied to
+/// the child widget.
+/// ## Example
+///
+/// ```dart
+/// class AnimateConstraints extends StatefulWidget {
+///   const AnimateConstraints({super.key});
+///
+///   @override
+///   State<AnimateConstraints> createState() => _AnimateConstraintsState();
+/// }
+///
+/// class _AnimateConstraintsState extends State<AnimateConstraints> {
+///   bool grow = true;
+///
+///   double maxHeight = 200;
+///   double maxWidth = 200;
+///   void toggle() {
+///     setState(() {
+///       grow = !grow;
+///       maxHeight = grow ? 400 : 200;
+///       maxWidth = grow ? 400 : 200;
+///     });
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Column(
+///       children: [
+///         ElevatedButton(onPressed: toggle, child: const Text('Toggle')),
+///         AnimatedConstrainedBox(
+///           constraints: BoxConstraints(
+///             minWidth: double.infinity,
+///             maxHeight: maxHeight,
+///           ),
+///           child: const ColoredBox(
+///             color: Colors.lightBlue,
+///             child: Align(
+///               child: Text('Hello world'),
+///             ),
+///           ),
+///         ),
+///       ],
+///     );
+///   }
+/// }
+/// ```
 class AnimatedConstrainedBox extends ImplicitlyAnimatedWidget {
-  /// Creates a container that animates its parameters implicitly.
   AnimatedConstrainedBox({
     required this.child,
     required this.constraints,
