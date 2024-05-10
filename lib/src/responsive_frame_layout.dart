68970e415d80af9c1662ffcfd3c35769bc741b44
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-05-10 23:55:04 +0930

    Feat: Added assert and granular layout

--- lib/src/responsive_frame_layout.dart
+++ lib/src/responsive_frame_layout.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:responsive_frame/responsive_frame.dart';
 
@@ -14,12 +13,19 @@ class ResponsiveFrameLayout extends StatelessWidget {
     this.persistentFrameConfig = FrameConfig.empty,
     this.backgroundColor,
     super.key,
-  });
+  }) : assert(
+          small != null ||
+              extraLarge != null ||
+              large != null ||
+              medium != null ||
+              extraSmall != null,
+          'At least one size parameter must be provided.',
+        );
 
   final FrameConfig Function(BuildContext context)? extraLarge;
   final FrameConfig Function(BuildContext context)? large;
   final FrameConfig Function(BuildContext context)? medium;
-  final FrameConfig Function(BuildContext context) small;
+  final FrameConfig Function(BuildContext context)? small;
   final FrameConfig Function(BuildContext context)? extraSmall;
   final Breakpoints breakpoints;
   final bool animations;
@@ -28,7 +34,7 @@ class ResponsiveFrameLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BreakpointDataWidget<LayoutSize>(
+    return BreakpointData(
       initialHandlers: {
         'frameconfig': BreakpointsHandler<FrameConfig Function(BuildContext)>(
           breakpoints: breakpoints,
@@ -102,88 +108,6 @@ class _FrameState extends State<_Frame> {
           bodyBottom: config.bodyBottom,
           bottom: config.bottom,
         ),
-      ),
-    );
-  }
-}
-
-class Textbox extends StatefulWidget {
-  const Textbox({
-    super.key,
-    this.isLong = true,
-  });
-  final bool isLong;
-
-  @override
-  State<Textbox> createState() => _TextboxState();
-}
-
-class _TextboxState extends State<Textbox> {
-  late Color color;
-
-  @override
-  void initState() {
-    color = generateRandomColor();
-    super.initState();
-  }
-
-  Color generateRandomColor() {
-    final random = Random();
-    return Color.fromARGB(
-      255,
-      random.nextInt(256),
-      random.nextInt(256),
-      random.nextInt(256),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ColoredBox(
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Text(widget.isLong ? longtext : 'short text'),
-      ),
-    );
-  }
-}
-
-const longtext = """
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.""";
-
-class BreakpointDataWidget<K extends Enum> extends StatelessWidget {
-  const BreakpointDataWidget({
-    required this.child,
-    this.initialHandlers = const {},
-    super.key,
-  });
-  final Widget child;
-  final Map<String, BreakpointsHandler<Object?>> initialHandlers;
-  // ignore: strict_raw_type
-
-  @override
-  Widget build(BuildContext context) {
-    return ResponsiveData<LayoutSize>(
-      notifier: ResponsiveDataChangeNotifier<LayoutSize>(
-        breakpoints: Breakpoints.defaultBreakpoints,
-        initialHandlers: initialHandlers,
-      ),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          ResponsiveData.of<LayoutSize>(context);
-
-          return child;
-        },
       ),
     );
   }
--- lib/src/responsive_frame_layout_granular.dart
+++ lib/src/responsive_frame_layout_granular.dart
@@ -1,84 +1,146 @@
-// import 'package:flutter/material.dart';
-// import 'package:responsive_frame/responsive_frame.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_frame/responsive_frame.dart';
 
-// class ResponsiveFrameLayoutGranular extends StatefulWidget {
-//   const ResponsiveFrameLayoutGranular({
-//     required this.mobileNormal,
-//     this.breakpoints = Breakpoints.defaultBreakpointsGranular,
-//     this.animations = true,
-//     super.key,
-//     this.desktopExtraLarge,
-//     this.desktopLarge,
-//     this.desktopNormal,
-//     this.desktopSmall,
-//     this.tabletExtraLarge,
-//     this.tabletLarge,
-//     this.tabletNormal,
-//     this.tabletSmall,
-//     this.mobileExtraLarge,
-//     this.mobileLarge,
-//     this.mobileSmall,
-//     this.persistentConfig = FrameConfig.empty,
-//     this.watch,
-//   });
-//   final FrameConfig Function(BuildContext context) mobileNormal;
-//   final FrameConfig Function(BuildContext context)? desktopExtraLarge;
-//   final FrameConfig Function(BuildContext context)? desktopLarge;
-//   final FrameConfig Function(BuildContext context)? desktopNormal;
-//   final FrameConfig Function(BuildContext context)? desktopSmall;
-//   final FrameConfig Function(BuildContext context)? tabletExtraLarge;
-//   final FrameConfig Function(BuildContext context)? tabletLarge;
-//   final FrameConfig Function(BuildContext context)? tabletNormal;
-//   final FrameConfig Function(BuildContext context)? tabletSmall;
-//   final FrameConfig Function(BuildContext context)? mobileExtraLarge;
-//   final FrameConfig Function(BuildContext context)? mobileLarge;
-//   final FrameConfig Function(BuildContext context)? mobileSmall;
-//   final FrameConfig Function(BuildContext context)? watch;
-//   final Breakpoints<ScreenSizeGranular> breakpoints;
-//   final bool animations;
-//   final FrameConfig persistentConfig;
+class ResponsiveFrameLayoutGranular extends StatelessWidget {
+  const ResponsiveFrameLayoutGranular({
+    this.breakpoints = BreakpointsGranular.defaultBreakpoints,
+    this.animations = true,
+    super.key,
+    this.jumboExtraLarge,
+    this.jumboLarge,
+    this.jumboNormal,
+    this.jumboSmall,
+    this.standardExtraLarge,
+    this.standardLarge,
+    this.standardNormal,
+    this.standardSmall,
+    this.compactExtraLarge,
+    this.compactLarge,
+    this.compactNormal,
+    this.compactSmall,
+    this.persistentFrameConfig = FrameConfig.empty,
+    this.tiny,
+    this.backgroundColor,
+  }) : assert(
+          jumboExtraLarge != null ||
+              jumboLarge != null ||
+              jumboNormal != null ||
+              jumboSmall != null ||
+              standardExtraLarge != null ||
+              standardLarge != null ||
+              standardNormal != null ||
+              standardSmall != null ||
+              compactExtraLarge != null ||
+              compactLarge != null ||
+              compactNormal != null ||
+              compactSmall != null ||
+              tiny != null,
+          'At least one size parameter must be provided.',
+        );
 
-//   @override
-//   State<ResponsiveFrameLayoutGranular> createState() =>
-//       _ResponsiveFrameLayoutState();
-// }
+  final FrameConfig Function(BuildContext context)? jumboExtraLarge;
+  final FrameConfig Function(BuildContext context)? jumboLarge;
+  final FrameConfig Function(BuildContext context)? jumboSmall;
+  final FrameConfig Function(BuildContext context)? jumboNormal;
+  final FrameConfig Function(BuildContext context)? standardExtraLarge;
+  final FrameConfig Function(BuildContext context)? standardLarge;
+  final FrameConfig Function(BuildContext context)? standardNormal;
+  final FrameConfig Function(BuildContext context)? standardSmall;
+  final FrameConfig Function(BuildContext context)? compactExtraLarge;
+  final FrameConfig Function(BuildContext context)? compactLarge;
+  final FrameConfig Function(BuildContext context)? compactNormal;
+  final FrameConfig Function(BuildContext context)? compactSmall;
+  final FrameConfig Function(BuildContext context)? tiny;
+  final BreakpointsGranular breakpoints;
+  final bool animations;
+  final FrameConfig persistentFrameConfig;
+  final Color? backgroundColor;
 
-// class _ResponsiveFrameLayoutState extends State<ResponsiveFrameLayoutGranular> {
+  @override
+  Widget build(BuildContext context) {
+    return BreakpointDataGranular(
+      initialHandlers: {
+        'frameconfig':
+            BreakpointsHandlerGranular<FrameConfig Function(BuildContext)>(
+          breakpoints: breakpoints,
+          jumboExtraLarge: jumboExtraLarge,
+          jumboLarge: jumboLarge,
+          jumboNormal: jumboNormal,
+          jumboSmall: jumboSmall,
+          standardExtraLarge: standardExtraLarge,
+          standardLarge: standardLarge,
+          standardNormal: standardNormal,
+          standardSmall: standardSmall,
+          compactExtraLarge: compactExtraLarge,
+          compactLarge: compactLarge,
+          compactNormal: compactNormal,
+          compactSmall: compactSmall,
+          tiny: tiny,
+        ),
+      },
+      child: _Frame(
+        persistentFrameConfig: persistentFrameConfig,
+        backgroundColor: backgroundColor,
+        animations: animations,
+      ),
+    );
+  }
+}
 
-//   bool _isInit = true;
-//   @override
-//   void initState() {
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       setInitStatus(false);
-//     });
-//     super.initState();
-//   }
+class _Frame extends StatefulWidget {
+  const _Frame({
+    required this.persistentFrameConfig,
+    required this.animations,
+    this.backgroundColor,
+  });
+  final Color? backgroundColor;
+  final FrameConfig persistentFrameConfig;
+  final bool animations;
+  @override
+  State<_Frame> createState() => _FrameState();
+}
 
-//   void setInitStatus(bool isInit) {
-//     _isInit = isInit;
-//   }
+class _FrameState extends State<_Frame> {
+  bool _isInit = true;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setInitStatus();
+    });
+    super.initState();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return LayoutBuilder(
-//       builder: (context, constraints) {
-//         final config = controller
-//             .breakpointCallback(context)
-//             .merge(widget.persistentConfig);
-//         return Frame(
-//           dimensions: config.dimensions,
-//           animations: !_isInit && widget.animations,
-//           body: config.body ?? const SizedBox(),
-//           top: config.top,
-//           bodyTop: config.bodyTop,
-//           leftEnd: config.leftEnd,
-//           leftEndTop: config.leftEndTop,
-//           rightEnd: config.rightEnd,
-//           rightEndTop: config.rightEndTop,
-//           bodyBottom: config.bodyBottom,
-//           bottom: config.bottom,
-//         );
-//       },
-//     );
-//   }
-// }
+  void setInitStatus() {
+    _isInit = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final handler = ResponsiveData.handlerOf<FrameConfig Function(BuildContext),
+        LayoutSizeGranular>(
+      context,
+      'frameconfig',
+      MediaQuery.sizeOf(context).width,
+    );
+    final config = handler.call(context).merge(widget.persistentFrameConfig);
+
+    return Material(
+      color: widget.backgroundColor,
+      child: SafeArea(
+        child: Frame(
+          dimensions: config.dimensions,
+          animations: !_isInit && widget.animations,
+          body: config.body ?? const SizedBox(),
+          top: config.top,
+          bodyTop: config.bodyTop,
+          leftEnd: config.leftEnd,
+          leftEndTop: config.leftEndTop,
+          rightEnd: config.rightEnd,
+          rightEndTop: config.rightEndTop,
+          bodyBottom: config.bodyBottom,
+          bottom: config.bottom,
+        ),
+      ),
+    );
+  }
+}
