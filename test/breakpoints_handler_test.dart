2400e610dc20937bede6d7e751883ff1f963cbd2
ericwimp <166397898+ericwimp8@users.noreply.github.com>
2024-05-11 02:30:16 +0930

    Add BreakpoinstHandler test

---
+++ test/breakpoints_handler_test.dart
@@ -0,0 +1,185 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:responsive_frame/responsive_frame.dart';
+
+void main() {
+  group('BaseBreakpointsHandler', () {
+    test(
+        'getLayoutSizeValue should return correct value, when all values are supplied.',
+        () {
+      const breakpoints = Breakpoints.defaultBreakpoints;
+
+      final handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        extraLarge: 10,
+        large: 9,
+        medium: 8,
+        small: 7,
+        extraSmall: 6,
+      );
+
+      var value = handler.getLayoutSizeValue(1200);
+      expect(value, 10);
+      value = handler.getLayoutSizeValue(1199);
+      expect(value, 9);
+      value = handler.getLayoutSizeValue(950);
+      expect(value, 9);
+      value = handler.getLayoutSizeValue(949);
+      expect(value, 8);
+      value = handler.getLayoutSizeValue(600);
+      expect(value, 8);
+      value = handler.getLayoutSizeValue(599);
+      expect(value, 7);
+      value = handler.getLayoutSizeValue(300);
+      expect(value, 7);
+      value = handler.getLayoutSizeValue(299);
+      expect(value, 6);
+    });
+
+    test(
+        'getLayoutSizeValue should return correct value, when only single values supplied.',
+        () {
+      const breakpoints = Breakpoints.defaultBreakpoints;
+
+      var handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        extraLarge: 10,
+      );
+
+      var value = handler.getLayoutSizeValue(1200);
+      expect(value, 10);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        large: 9,
+      );
+
+      value = handler.getLayoutSizeValue(950);
+      expect(value, 9);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        large: 8,
+      );
+
+      value = handler.getLayoutSizeValue(600);
+      expect(value, 8);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        medium: 7,
+      );
+
+      value = handler.getLayoutSizeValue(300);
+      expect(value, 7);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        medium: 6,
+      );
+
+      value = handler.getLayoutSizeValue(299);
+      expect(value, 6);
+    });
+
+    test(
+        'getLayoutSizeValue should return the next lowest value when value is not supplied for the current breakpoint.',
+        () {
+      const breakpoints = Breakpoints.defaultBreakpoints;
+
+      var handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        large: 9,
+      );
+
+      var value = handler.getLayoutSizeValue(1200);
+      expect(value, 9);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        medium: 8,
+      );
+
+      value = handler.getLayoutSizeValue(950);
+      expect(value, 8);
+
+      handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        extraSmall: 7,
+      );
+
+      value = handler.getLayoutSizeValue(1200);
+      expect(value, 7);
+    });
+
+    test(
+        'getLayoutSizeValue should return the next highest value when current value is null and all lower values are null.',
+        () {
+      const breakpoints = Breakpoints.defaultBreakpoints;
+
+      final handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        large: 9,
+      );
+
+      var value = handler.getLayoutSizeValue(600);
+      expect(value, 9);
+      value = handler.getLayoutSizeValue(300);
+      expect(value, 9);
+      value = handler.getLayoutSizeValue(299);
+      expect(value, 9);
+    });
+
+    test('getLayoutSizeValue should throw an assertion error when size < 0.',
+        () {
+      const breakpoints = Breakpoints.defaultBreakpoints;
+
+      final handler = BreakpointsHandler<int>(
+        breakpoints: breakpoints,
+        large: 9,
+      );
+
+      expect(() => handler.getLayoutSizeValue(-1), throwsAssertionError);
+    });
+
+    test(
+        'BreakpointsHandler should throw assertion error when all values are null.',
+        () {
+      BreakpointsHandler<int> createHandler() {
+        return BreakpointsHandler<int>(
+          breakpoints: Breakpoints.defaultBreakpoints,
+        );
+      }
+
+      expect(
+        createHandler,
+        throwsAssertionError,
+      );
+    });
+    test('getScreenSize should return the correct screen size.', () {
+      final handler = BreakpointsHandler<int>(
+        breakpoints: Breakpoints.defaultBreakpoints,
+        large: 9,
+      );
+
+      expect(handler.getScreenSize(1200), LayoutSize.extraLarge);
+      expect(handler.getScreenSize(1199), LayoutSize.large);
+      expect(handler.getScreenSize(950), LayoutSize.large);
+      expect(handler.getScreenSize(949), LayoutSize.medium);
+      expect(handler.getScreenSize(600), LayoutSize.medium);
+      expect(handler.getScreenSize(599), LayoutSize.small);
+      expect(handler.getScreenSize(300), LayoutSize.small);
+      expect(handler.getScreenSize(299), LayoutSize.extraSmall);
+      expect(handler.getScreenSize(0), LayoutSize.extraSmall);
+    });
+
+    test('getScreenSize should return smallest screen size when size is -1.',
+        () {
+      final handler = BreakpointsHandler<int>(
+        breakpoints: Breakpoints.defaultBreakpoints,
+        large: 9,
+      );
+
+      expect(handler.getScreenSize(-1), LayoutSize.extraSmall);
+    });
+  });
+}
--- test/responsive_frame_test.dart
+++ test/responsive_frame_test.dart
@@ -1,4 +1,5 @@
 import 'animated_show_hide_test.dart' as animated_show_hide_test;
+import 'breakpoints_handler_test.dart' as breakpoints_handler_test;
 import 'responsive_widget_granular_test.dart'
     as responsive_widget_granular_test;
 import 'responsive_widget_test.dart' as responsive_widget_test;
@@ -7,4 +8,5 @@ void main() {
   animated_show_hide_test.main();
   responsive_widget_test.main();
   responsive_widget_granular_test.main();
+  breakpoints_handler_test.main();
 }
